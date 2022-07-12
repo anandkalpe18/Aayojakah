6750a4d55149f49e3a25e66ebf5840dcc681fd57
Anand Kalpe <61860913+anandkalpe18@users.noreply.github.com>
2022-07-12 18:54:51 +0530

    Add files via upload

---
+++ Aayojakah/Controllers/JWTApiController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Aayojakah.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aayojakah.Controllers
+{
+	public class LoginController : Controller
+	{
+		private IConfiguration _config;
+		public LoginController(IConfiguration config)
+		{
+			_config = config;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		public IActionResult Login([FromBody] User login)
+		{
+			IActionResult response = Unauthorized();
+			var user = AuthenticateUser(login);
+
+			if (user != null)
+			{
+				var tokenString = GenerateJSONWebToken(user);
+				response = Ok(new { token = tokenString });
+			}
+
+			return response;
+		}
+
+		private string GenerateJSONWebToken(User userInfo)
+		{
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+			var claims = new[] {
+		new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+		new Claim(JwtRegisteredClaimNames.Email, userInfo.Password),
+		new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+	};
+
+			var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+			  _config["Jwt:Issuer"],
+			  null,
+			  expires: DateTime.Now.AddMinutes(120),
+			  signingCredentials: credentials);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+
+		private User AuthenticateUser(User login)
+		{
+			User user = null;
+
+
+			if (login.UserName == user.UserName)
+			{
+				user = new User { UserName = user.UserName, Password = user.Password };
+			}
+			return user;
+		}
+	}
+
+}
